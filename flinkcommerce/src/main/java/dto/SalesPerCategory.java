02fcdeb3b7c727f6395370db461b40b79c049f4b
darshan601 <darshanmistry11@gmail.com>
2024-06-13 17:54:07 -0700

    First Commit

---
+++ flinkcommerce/src/main/java/dto/SalesPerCategory.java
@@ -0,0 +1,16 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class SalesPerCategory {
+
+    private Date transactionDate;
+    private String category;
+    private Double totalSales;
+
+}
---
+++ flinkcommerce/src/main/java/dto/SalesPerMonth.java
@@ -0,0 +1,14 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SalesPerMonth {
+    private int year;
+    private int month;
+    private double totalSales;
+
+
+}
